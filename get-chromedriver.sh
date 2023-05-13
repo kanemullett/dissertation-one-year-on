066331ccdd494fb27dcf6a2eebd890fe9cdf3d24
Kane Mullett <kane.mullett@savient.uk.com>
2023-05-13 21:25:22 +0100

    Created chromedriver installation script.

---
+++ get-chromedriver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl https://chromedriver.storage.googleapis.com/113.0.5672.63/chromedriver_win32.zip --output chromedriver.zip
+
+unzip chromedriver.zip
+
+rm chromedriver.zip
+
+rm LICENSE.chromedriver

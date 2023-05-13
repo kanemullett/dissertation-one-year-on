5a956dd91e37128840a5e6d8c0ace0ebc4abf8f3
Kane Mullett <kane.mullett@savient.uk.com>
2023-05-13 20:08:52 +0100

    Added docstrings for selenium scraper.

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -4,6 +4,10 @@ from selenium.webdriver.common.by import By
 
 
 class SeleniumScraper:
+    """
+    Selenium Scraper object for scraping web pages for data.
+    """
+
     def __init__(self):
         self.options = Options()
         self.options.headless = True
@@ -14,6 +18,15 @@ class SeleniumScraper:
         )
 
     def __get_fixtures_lines(self, url: str) -> list[str]:
+        """
+        Scrape the lines of a fixtures table from Basketball-Reference.
+
+        :param url: The URL of the page upon which the table is displayed.
+        :type url: str
+        :return: Lines of fixtures table.
+        :rtype: list[str]
+        """
+
         self.driver.get(url)
         table_web_element = self.driver.find_element(By.ID, "schedule")
         table_text = table_web_element.text
@@ -21,9 +34,27 @@ class SeleniumScraper:
         return table_text.split("\n")
 
     def scrape_fixtures(self, url: str) -> list[str]:
+        """
+        Scrape the lines of a fixtures table from Basketball-Reference.
+
+        :param url: The URL of the page upon which the table is displayed.
+        :type url: str
+        :return: Lines of fixtures table.
+        :rtype: list[str]
+        """
+
         return self.__get_fixtures_lines(url)
 
     def __get_statistics_lines(self, url: str) -> list[str]:
+        """
+        Scrape the lines of a statistics table from Basketball-Reference.
+
+        :param url: The URL of the page upon which the table is displayed.
+        :type url: str
+        :return: Lines of statistics table.
+        :rtype: list[str]
+        """
+
         self.driver.get(url)
         table_web_element = self.driver.find_element(By.ID, "team_and_opponent")
         table_text = table_web_element.text
@@ -31,4 +62,13 @@ class SeleniumScraper:
         return table_text.split("\n")
 
     def scrape_statistics(self, url: str) -> list[str]:
+        """
+        Scrape the lines of a statistics table from Basketball-Reference.
+
+        :param url: The URL of the page upon which the table is displayed.
+        :type url: str
+        :return: Lines of statistics table.
+        :rtype: list[str]
+        """
+
         return self.__get_statistics_lines(url)

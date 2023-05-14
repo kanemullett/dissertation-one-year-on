ebc2b8608d291801cba6580c7bb282e1bacc401b
Kane Mullett <kane.mullett@savient.uk.com>
2023-05-14 01:01:31 +0100

    Created initial flask app.

--- data-retrieval/app.py
+++ data-retrieval/app.py
@@ -1,2 +1,14 @@
+import os
+
 from flask import Flask, Response, jsonify, make_response, request
-from flask_restx import Api,Resource, fields
+from flask_restx import Api, Resource, fields
+
+app = Flask(__name__)
+
+api = Api(app, title="Data Retrieval Service Controller", version="0.0.1")
+
+scrape = api.namespace("data-retrieval")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
